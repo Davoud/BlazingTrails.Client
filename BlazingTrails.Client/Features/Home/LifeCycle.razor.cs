9d9080de0bddd1b769a6d2888aaf53a9474b7251
Davoud Nasiri Mobarakeh <nas.david@yahoo.com>
2023-02-11 20:38:43 +0330

    Added TrailDetails

---
+++ BlazingTrails.Client/Features/Home/LifeCycle.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazingTrails.Client.Features.Home;
+
+public partial class LifeCycle
+{
+    List<string> _greeting = new List<string>();
+
+    public override async Task SetParametersAsync(ParameterView parameters)
+    {
+        Console.WriteLine("SetParametersAsync - Begin");
+        await base.SetParametersAsync(parameters);
+        Console.WriteLine("SetParametersAsync - End");
+    }
+
+    protected override void OnInitialized()                
+        => Console.WriteLine("OnInitialized");
+ 
+    protected override async Task OnInitializedAsync()
+    {
+        _greeting.Add("Welcome");            
+ 
+        await Task.Delay(1000);              
+        _greeting.Add("to");
+        StateHasChanged();
+        await Task.Delay(1000);              
+        _greeting.Add("Blazor in Action");   
+    }
+ 
+    protected override void OnParametersSet()              
+        => Console.WriteLine("OnParametersSet");
+
+    protected override async Task OnParametersSetAsync()
+    {
+        Console.WriteLine("OnParametersSetAsync - Begin");
+        await Task.Delay(500);
+        Console.WriteLine("OnParametersSetAsync - End");
+    }
+
+    protected override void OnAfterRender(bool firstRender)                                   
+        => Console.WriteLine($"OnAfterRender (First render: {firstRender})");
+ 
+    protected override async Task OnAfterRenderAsync(bool firstRender)                                   
+        => Console.WriteLine($"OnAfterRenderAsync (First render: {firstRender})");
+}
+
