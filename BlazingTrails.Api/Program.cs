8d8474d14fa5abf6580c986629b2b30a7b102c39
Davoud Nasiri Mobarakeh <nas.david@yahoo.com>
2023-02-20 00:14:47 +0330

    Adding backend phase 1

---
+++ BlazingTrails.Api/Program.cs
@@ -0,0 +1,36 @@
+
+namespace BlazingTrails.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapControllers();
+            app.MapFallbackToFile("index.html");
+
+            app.Run();
+
+        }
+    }
+}
